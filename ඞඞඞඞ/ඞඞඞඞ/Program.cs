f16568513cc4315af159db7638ade207350916e9
TibsT <notjimbo78@gmail.com>
2023-06-08 15:02:28 +0100

    First Day
    i wanna learn pandas :(

---
+++ ඞඞඞඞ/ඞඞඞඞ/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var hellomate = "i wanna learn pandas pls";
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine(hellomate);
