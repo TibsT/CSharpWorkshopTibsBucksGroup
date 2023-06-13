2695e89c30189486d6ff72d0b92d686b3d6c645f
TibsT <notjimbo78@gmail.com>
2023-06-13 12:02:16 +0100

    Day 1
    SuperHeroName and that

---
+++ Day1App/Day1App/Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+
+//create a string variable
+
+string myName = "Sara";
+
+
+
+//creating another variable
+
+string superHeroName = "The Flash";
+string secondMovieName = "AdiPurush";
+
+//Console.WriteLine(superHeroName);
+
+
+
+//building the output string
+//+ is the concatenation operator
+
+string outputString = "This thursday, I will be watching the movie, " + superHeroName;
+
+Console.WriteLine(outputString);
+
+string outputString2 = "This friday, I will be watching the movie, " + secondMovieName;
+
+Console.WriteLine(outputString2);
+
+
+
+//create a variable to store an integer
+
+int myAge = 40;
+
+//Console.WriteLine(myAge);
+
+
+
+//create a decimal variable
+
+decimal myMoney = 100.50m;
+
+//Console.WriteLine(myMoney);
+
+
+
+//create a double variable
+
+double myDouble = 100.50;
+
+//Console.WriteLine(myDouble);
+
+
+
+//create a boolean variable
+
+bool isRaining = true;
+
+//Console.WriteLine(isRaining);
