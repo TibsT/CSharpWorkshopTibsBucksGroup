93d1c54a82c233ce7f8e735cf64231d2cf0c8194
TibsT <notjimbo78@gmail.com>
2023-06-20 11:55:57 +0100

    Day 3 or smth
    yeah

--- Day1App/Day1App/Program.cs
+++ Day1App/Day1App/Program.cs
@@ -2,10 +2,9 @@
 //DAY 1
 //==================
 
+
 // See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
-
-
 
 //create a string variable
 
@@ -67,8 +66,9 @@ bool isRaining = true;
 
 
 //==================
-//Day 2
+//DAY 2
 //==================
+
 
 //two variables that hold two numbers
 
@@ -129,3 +129,102 @@ int AddTwoNumbers(int num1, int num2)
 //Function Call
 
 int storeResultOfAddTwoNumbersFunctionCall = AddTwoNumbers(numberOneForFunctions, numberTwoForFunctions);
+
+
+//==================
+//DAY 3
+//==================
+
+
+//i wish to write a function that
+//does not take any arguments
+//does not return anything
+
+void FunctionTypeOne()
+{
+    string someString1 = "Hey guys, the Flash movie is not that good";
+    Console.WriteLine(someString1);
+}
+
+//Function Call+
+FunctionTypeOne();
+
+//Type Two
+//i wish to write a function that
+//that will take single integer parameter
+//and does not return anything
+
+//function definition
+void FunctionTypeTwo(int numberofmovies)
+{
+    string outputSentence = $"I have watched {numberofmovies} movies this year";
+    Console.WriteLine(outputSentence);
+}
+
+int tobeSentToFunctionMovieCount = 200;
+
+FunctionTypeTwo(tobeSentToFunctionMovieCount);
+
+FunctionTypeTwo(10);
+
+//same as type two but with more than one parameter
+
+void FunctionTypeTwoPart8(int one, string two, bool three)
+{
+    string someOutput = $"The values are {one}, {two}, {three}";
+    Console.WriteLine(someOutput);
+}
+
+int one = 1;
+string two = "yes";
+bool three = true;
+
+FunctionTypeTwoPart8(one, two, three);
+
+//Type Three
+//a function that is taking some parameters
+//and returning some value
+
+//function definition
+
+//function that will add two numbers
+int FunctionThatAddsTwoNumbers(int numberOne, int numberTwo)
+{
+    int sum = numberOne + numberTwo;
+    return sum;
+}
+
+int doubleooseven = 7;
+int randomagent = 50;
+
+//function call
+
+int result = FunctionThatAddsTwoNumbers(doubleooseven, randomagent);
+
+string outputofAgents = $"The sum of {doubleooseven} and {randomagent} is {result}";
+Console.WriteLine(outputofAgents);
+
+//Type Four
+
+//a function that is NOT taking any parameters
+//and returning a value
+
+//function definition
+int FunctionThatReturnsRandomNumber()
+{
+    Random random = new Random();
+    int randomNumber = random.Next(1, 100);
+    return randomNumber;
+}
+
+//function call
+//i simply want a random number, between 1 and 100
+
+int randomnumber = FunctionThatReturnsRandomNumber();
+string somerandomsentence = $"The random number is {randomnumber} between 1 and 100, no parameters are sent";
+
+//i want a random number betwen 500 and 800
+int lowerboundvalue = 500;
+int upperboundvalue = 800;
+//int randomnumber2 = FunctionThatReturnsRandomNumber(500, 800);
+
