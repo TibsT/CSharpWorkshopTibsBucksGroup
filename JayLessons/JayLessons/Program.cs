25f92f073f038cc0a79924edd4e33fc8cbe6de1e
TibsT <notjimbo78@gmail.com>
2023-06-27 12:06:27 +0100

    day5 again
    idk

--- JayLessons/JayLessons/Program.cs
+++ JayLessons/JayLessons/Program.cs
@@ -32,3 +32,20 @@ KitWalker.StudyWeeksCompleted = 30;
 
 KitWalker.BuildStudentInformation();
 KitWalker.DisplayStudentInformation();
+
+//500 hours is the same amount of time it takes to become
+//a beginner developer.
+
+int TotalNumberOfHoursToBecomeDeveloper = 500;
+
+//20 hours per week
+//25 weeks to become a beginner developer.
+//number of weeks already completed is 0
+//remaining weeks = 25-0 = 25 weeks
+//ultimately, batman will become developer in 25 weeks.
+
+//10 hours per week
+//50 weeks to become a beginner developer
+//10 weeks are already completed
+//remaining weeks = 50-10 = 40 weeks.
+//eventually, catwoman will be a developer in 40 weeks.
