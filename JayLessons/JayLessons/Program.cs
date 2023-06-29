884e48a0fb770cf5046b58f6d74824390c058b54
TibsT <notjimbo78@gmail.com>
2023-06-29 12:02:45 +0100

    Day6 i think
    yeah

--- JayLessons/JayLessons/Program.cs
+++ JayLessons/JayLessons/Program.cs
@@ -7,6 +7,7 @@ Console.WriteLine("Hello, World!");
 
 Student BruceWayne = new Student();
 Student SelinaKyle = new Student();
+Student Tibi = new Student();
 
 BruceWayne.NameOfStudent = "Bruce Wayne";
 BruceWayne.StudyHoursPerWeek = 20;
@@ -33,6 +34,10 @@ KitWalker.StudyWeeksCompleted = 30;
 KitWalker.BuildStudentInformation();
 KitWalker.DisplayStudentInformation();
 
+Tibi.NameOfStudent = "Tibi";
+Tibi.StudyHoursPerWeek = 2;
+Tibi.StudyWeeksCompleted = 10;
+
 //500 hours is the same amount of time it takes to become
 //a beginner developer.
 
@@ -49,3 +54,31 @@ int TotalNumberOfHoursToBecomeDeveloper = 500;
 //10 weeks are already completed
 //remaining weeks = 50-10 = 40 weeks.
 //eventually, catwoman will be a developer in 40 weeks.
+
+//int numberOfWeeksBasedOnStudentWeeklyHoursCommitment = TotalNumberOfHoursToBecomeDeveloper / BruceWayne.StudyHoursPerWeek;
+
+//Console.WriteLine(numberOfWeeksBasedOnStudentWeeklyHoursCommitment);
+
+//int remainingnumberOfWeeksToStudy = numberOfWeeksBasedOnStudentWeeklyHoursCommitment - BruceWayne.StudyWeeksCompleted;
+
+//Console.WriteLine(remainingnumberOfWeeksToStudy);
+
+//string outputforBatmanStudent = $"Student Name: {BruceWayne.NameOfStudent} will need to study for {remainingnumberOfWeeksToStudy} to become a basic beginner developer";
+
+//Console.Write(outputforBatmanStudent);
+
+//here, i want to start using the developer class
+
+Developer DeveloperTesting = new Developer();
+
+DeveloperTesting.CalculatorDeveloperEstimate(BruceWayne.StudyHoursPerWeek, BruceWayne.StudyWeeksCompleted, BruceWayne.NameOfStudent);
+DeveloperTesting.DisplayDeveloperEstimate();
+
+DeveloperTesting.CalculatorDeveloperEstimate(SelinaKyle.StudyHoursPerWeek, SelinaKyle.StudyWeeksCompleted, SelinaKyle.NameOfStudent);
+DeveloperTesting.DisplayDeveloperEstimate();
+
+DeveloperTesting.CalculatorDeveloperEstimate(KitWalker.StudyHoursPerWeek, KitWalker.StudyWeeksCompleted, KitWalker.NameOfStudent);
+DeveloperTesting.DisplayDeveloperEstimate();
+
+DeveloperTesting.CalculatorDeveloperEstimate(Tibi.StudyHoursPerWeek, Tibi.StudyWeeksCompleted, Tibi.NameOfStudent);
+DeveloperTesting.DisplayDeveloperEstimate();
