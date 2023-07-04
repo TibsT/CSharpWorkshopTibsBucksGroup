a7b767c17bfa40f6ef77889e76b29da7d2762713
TibsT <notjimbo78@gmail.com>
2023-07-04 11:31:49 +0100

    Final Session Part 1

---
+++ Final Session/Final Session/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int someRandomNumber = 55;
+
+string BooleanPositive = "Yes, the number is 55";
+string BooleanNegative = "No, the number is not 55";
+
+if (someRandomNumber == 55)
+{
+    Console.WriteLine(BooleanPositive);
+}
+else
+{
+    Console.WriteLine(BooleanNegative);
+}
+
+if(someRandomNumber != 55)
+{
+    Console.WriteLine(BooleanNegative);
+}
+else
+{
+    Console.WriteLine(BooleanPositive);
+}
+
+//simple for loop that counts from 1 to 10
+
+int beginningNumber = 1;
+int endingNumber = 10;
+
+for(int i = beginningNumber; i <= endingNumber; i++)
+{
+    string sentence = $"I am counting at number {i}";
+
+    Console.WriteLine(sentence);
+}
